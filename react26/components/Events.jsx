b8f89322b2f05dddb1cbbb786b8b4eb40ad6e291
kartikeypatel1 <kp0543985@gmail.com>
2026-02-25 03:12:57 +0530

    events

---
+++ react26/components/Events.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Events() {
+    const handleClick = () => {
+        alert("Button Clicked!")
+    }   
+  return (
+    <div>
+      <h1>we are learning React </h1>
+        <button onMouseOver={handleClick}>Click Me</button>
+    </div>
+  )
+}
+
+export default Events
