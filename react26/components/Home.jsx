422de231283390b67ecb413686b04706789921cd
kartikeypatel1 <kp0543985@gmail.com>
2026-02-25 03:56:04 +0530

    started new project

---
+++ react26/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Person from '../components/Person'
+import Product from '../components/Product'
+import SongCard from '../components/Card'
+import Person3 from '../components/Person3'
+import Events from '../components/Events'
+import Counter from '../components/Counter'
+import ShowProducts from '../components/ShowProducts'
+import FilterProducts from '../components/FilterProducts'
+
+function Home() {
+  return (
+    <div>
+      // <>
+    // {/* <h1>Hallo Kartikey</h1>
+    // <Person /> */}
+    // {/* <Product image="https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D"name="Laptop" price={999} />
+    // <Product image="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww" name="Laptop Pro" price={1999} />
+    // <Product image="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww" name="Laptop Air" price={1499} />
+    // <Product image="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww" name="Laptop Mini" price={799} /> */}
+    // {/* <SongCard cover="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f" title="Midnight Echo" artist="Aria Moon"/>
+    // <SongCard cover="https://images.unsplash.com/photo-1506744038136-46273834b3fb" title="Sunset Serenade" artist="Leo Star"/>
+    // <SongCard cover="https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4" title="Ocean Breeze" artist="Mia Wave"/> */}
+    // {/* <Person3 name="Kartikey" age={20} /> */}
+    // <Events />
+    // </>
+    <>
+    {/* <Events /> */}
+    {/* <Counter /> */}
+    {/* <ShowProducts /> */}
+    <FilterProducts />
+        </>
+    
+    </div>
+  )
+}
+
+export default Home
